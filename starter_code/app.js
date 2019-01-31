012c1d324fe2beadb1fb0969be38ea13b0311e34
caigma <caigma@gmail.com>
2019-01-31 17:35:53 +0100

    pruebas pescando datos en api

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const bodyParser = require('body-parser');
 // require spotify-web-api-node package here:
-const SpotifyWebApi = required('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+hbs.registerPartials(__dirname + '/views/partials');
+
+// setting the spotify-api goes here:
 const clientId = '3ca06be35b9a4da882529f4610aca26f',
 	clientSecret = '0903a31644ae4d069ea7d466f77d1a65';
 
@@ -20,15 +30,23 @@ spotifyApi
 	.catch((error) => {
 		console.log('Something went wrong when retrieving an access token', error);
 	});
-
-const app = express();
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-
-// setting the spotify-api goes here:
-
 // the routes go here:
+app.get('/', (req, res, next) => {
+	res.render('index');
+});
+
+app.post('/artist', (req, res, next) => {
+	spotifyApi
+		.searchArtists(req.body.name)
+		.then((data) => {
+			res.render('artist', { data });
+
+			console.log('The received data from the API: ', data.body);
+			// ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+		})
+		.catch((err) => {
+			console.log('The error while searching artists occurred: ', err);
+		});
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
